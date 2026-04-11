21581ed9379f752fa507cb75714bc004b26cc03c
AdithyaRamachar <adithya.anytime.anywhere@gmail.com>
2026-04-12 00:05:29 +0530

    testing in local

--- app/model_loader.py
+++ app/model_loader.py
@@ -1,11 +1,24 @@
+import os
 import mlflow
 import mlflow.pyfunc
-from config import MLFLOW_TRACKING_URI, MODEL_NAME, MODEL_STAGE
+from config import (
+    MLFLOW_TRACKING_URI,
+    DAGSHUB_USERNAME,
+    DAGSHUB_TOKEN,
+    MODEL_NAME,
+    MODEL_STAGE
+)
+
+# 🔐 Set credentials
+os.environ["MLFLOW_TRACKING_USERNAME"] = DAGSHUB_USERNAME
+os.environ["MLFLOW_TRACKING_PASSWORD"] = DAGSHUB_TOKEN
 
 mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
+
 
 def load_model():
     model_uri = f"models:/{MODEL_NAME}/{MODEL_STAGE}"
     return mlflow.pyfunc.load_model(model_uri)
+
 
 model = load_model()
--- config.py
+++ config.py
@@ -1,3 +1,11 @@
-MLFLOW_TRACKING_URI = "https://dagshub.com/adithya.anytime.anywhere/Credit-Risk-Modelling.mlflow"
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI")
+DAGSHUB_USERNAME = os.getenv("DAGSHUB_USERNAME")
+DAGSHUB_TOKEN = os.getenv("DAGSHUB_TOKEN")
+
 MODEL_NAME = "Random_Forest_Tuned_Model"
 MODEL_STAGE = "Production"
