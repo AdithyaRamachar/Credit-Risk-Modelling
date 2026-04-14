0ca54cd73924f67214df8c8e1b2ac0e402c30a36
AdithyaRamachar <adithya.anytime.anywhere@gmail.com>
2026-04-15 00:15:51 +0530

    Mask MLFLOW tracking username and password
    Replaced sensitive MLFLOW tracking credentials with placeholders.

--- download_model.py
+++ download_model.py
@@ -1,6 +1,6 @@
 import os
-os.environ['MLFLOW_TRACKING_USERNAME'] = 'adithya.anytime.anywhere'
-os.environ['MLFLOW_TRACKING_PASSWORD'] = 'e1e5810da884bd5a7b1cac9184c0da99f6920cb3'
+os.environ['MLFLOW_TRACKING_USERNAME'] = '----------'
+os.environ['MLFLOW_TRACKING_PASSWORD'] = '-----------'
 
 import mlflow
 
