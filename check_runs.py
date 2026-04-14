c07402e3317aee8e343dc0ad2e4d3cbfd529a8b5
AdithyaRamachar <adithya.anytime.anywhere@gmail.com>
2026-04-15 00:15:25 +0530

    Obfuscate MLFLOW tracking credentials
    Updated environment variables for MLFLOW tracking credentials.

--- check_runs.py
+++ check_runs.py
@@ -1,6 +1,6 @@
 import os
-os.environ['MLFLOW_TRACKING_USERNAME'] = 'adithya.anytime.anywhere'
-os.environ['MLFLOW_TRACKING_PASSWORD'] = 'e1e5810da884bd5a7b1cac9184c0da99f6920cb3'
+os.environ['MLFLOW_TRACKING_USERNAME'] = '----------'
+os.environ['MLFLOW_TRACKING_PASSWORD'] = '-----------'
 import mlflow
 mlflow.set_tracking_uri('https://dagshub.com/adithya.anytime.anywhere/Credit-Risk-Modelling.mlflow')
 client = mlflow.tracking.MlflowClient()
